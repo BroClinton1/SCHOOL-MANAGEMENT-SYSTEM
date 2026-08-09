2520b59f5a3812fb025e25847918f2e559145363
Clinton <hebrewclinton5@gamil.com>
2026-08-09 22:50:46 +0100

    modified splash

--- frontend/src/pages/auth/Splash.js
+++ frontend/src/pages/auth/Splash.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Splash.css";
 
 const Splash = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/login");
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [navigate]);
+
   return (
     <main className="splash-page">
       <div className="splash-content">
