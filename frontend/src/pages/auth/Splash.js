16bb9e2ee736c6358a0f1faf383e61f032f5102f
Clinton <hebrewclinton5@gamil.com>
2026-08-09 08:09:53 +0100

    built the splash screen

--- frontend/src/pages/auth/Splash.js
+++ frontend/src/pages/auth/Splash.js
@@ -1,11 +1,36 @@
 import React from "react";
+import "./Splash.css";
 
 const Splash = () => {
   return (
-    <div>
-      <h1>EduSmart</h1>
-      <p>School Management System</p>
-    </div>
+    <main className="splash-page">
+      <div className="splash-content">
+
+        {/* Logo */}
+        <div className="splash-logo">
+          <div className="splash-logo-shield">
+            C
+          </div>
+        </div>
+
+        {/* Application name */}
+        <h1 className="splash-title">
+          Clinstack
+        </h1>
+
+        <p className="splash-subtitle">
+          School Management System
+        </p>
+
+        {/* Loading indicator */}
+        <div className="splash-loader">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+      </div>
+    </main>
   );
 };
 
